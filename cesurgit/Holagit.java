e041f28fdc649130c55c1d617d9f47f5541ee0e1
ruben <ruben@DESKTOP-5KJ20GB>
2023-04-18 19:16:02 +0200

    primer commit

---
+++ cesurgit/Holagit.java
@@ -0,0 +1,13 @@
+package cesurgit;
+
+public class Holagit {
+
+	public static void main(String[] args) {
+        System.out.println("¡Hola, Git!");
+        
+        System.out.println("Actualización 1");
+        System.out.println("Actualización 2");
+        System.out.println("Actualización 3");
+	}
+
+}
